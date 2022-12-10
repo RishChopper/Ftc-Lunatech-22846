e76f0e10b90d979f2e9a0a23de98a0863d66a1d6
Rishabh Chopra <rishabhchopra.personal@gmail.com>
2022-12-10 14:26:16 +0530

    Basic Autonomous Program + Calibrated

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicAuto.java
@@ -1,12 +1,9 @@
 package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.util.Range;
-import com.qualcomm.robotcore.util.RobotLog;
 
 @Autonomous(name="Auto", group="TeamX")
-public class TeamAuto extends LinearOpMode {
+public class BasicAuto extends LinearOpMode {
 
     private TeamHardware robot;
 
@@ -21,7 +18,7 @@ public class TeamAuto extends LinearOpMode {
         // Step through each leg of the path,
         // Note: Reverse movement is obtained by setting a negative distance (not speed)
 
-        robot.encoderDrive(5, DataHolder.MOVEDIR.FRONT_RIGHT, 0, 5);
+        robot.encoderDrive(5, DataHolder.MOVEDIR.FRONT, 12, 5);
         telemetry.addData("Path", "Complete");
         telemetry.update();
     }
